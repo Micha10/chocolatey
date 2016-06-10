0a432355a32974cf851a60e3dfcf2ae97d9c8386
Micha10 <misifusc-git@yahoo.de>
2016-06-10 20:36:06 +0200

    Visipics. Zustimmung von Author steht noch aus

---
+++ DocFetcher/docfetcher.1.1.18 _LeseReadme.txt/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'DocFetcher' 
+$registryUninstallerKeyName = 'DocFetcher' 
+$installerType = 'EXE' 
+$url = 'http://sourceforge.net/projects/docfetcher/files/docfetcher/1.1.17/docfetcher_1.1.17_win32_setup.exe/download' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Stop-Process -Name docfetcher-daemon-windows.exe -Confirm -Debug -ErrorAction SilentlyContinue
+Stop-Process -Name DocFetcher.exe -Confirm -Debug -ErrorAction SilentlyContinue
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
