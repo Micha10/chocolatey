f4b906941b1308ad81b02a0399fbe85813aab14f
Micha10 <misifusc-git@yahoo.de>
2016-06-15 19:06:53 +0200

    Visipics hinzugefügt

---
+++ Visipics/Visipics131/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$realdownloadurl           = Get-UrlFromFosshub 'http://www.fosshub.com/genLink/VisiPics/VisiPics-1-31.exe'
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $realdownloadurl
+  
+  silentArgs    = "/verysilent /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'visipics*'
+
+}
+Install-ChocolateyPackage @packageArgs
