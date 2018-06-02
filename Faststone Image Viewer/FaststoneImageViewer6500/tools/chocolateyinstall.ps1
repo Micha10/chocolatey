95198405436e1925a55f08bc2c1a0c35a10e343d
mikado <mikado@localdomain.local>
2018-06-02 16:06:05 +0200

    20180602

---
+++ Faststone Image Viewer/FaststoneImageViewer6500/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'fsviewer'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.faststonesoft.net/DN/FSViewerSetup65.exe'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+
+  softwareName  = 'FastStone Image Viewer'
+
+  checksum      = '69a355c656a325ae6b2310ca1c42434e141703cfae5f6275272c7f20a4838af9'
+  checksumType  = 'sha256'
+  
+
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
