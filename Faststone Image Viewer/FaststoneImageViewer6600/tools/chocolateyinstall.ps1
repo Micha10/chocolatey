22a9a6b4745fc526c54f1b60b695d6782d77ca72
Mikado <Mikado1@RYZEN>
2018-09-29 16:13:44 +0200

    Entfernung der Source. Das ist nur bei Opensource nötig. Es soll nicht der Source dieses Packages angegeben werden

---
+++ Faststone Image Viewer/FaststoneImageViewer6600/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'fsviewer'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://www.faststonesoft.net/DN/FSViewerSetup66.exe'
+$checksum32 = '2c8eccb75a40d7691884be0d3d490280253e907694aa20e6f3a38dd387549b43'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+
+  softwareName  = 'FastStone Image Viewer'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  
+
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
