5408e0f7c00a755a3ccd3dac8f3c2ad90ef1e6c4
Mikado <Mikado1@RYZEN>
2018-11-06 00:13:28 +0100

    renamed old ones. corrected automatic name

---
+++ Faststone Image Viewer/fsviewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'fsviewer'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://www.faststonesoft.net/DN/FSViewerSetup67.exe'
+$checksum32 = 'fea31a30ea7047c59f18dd4a90c1a5a9a5e30fdad387a0d7ebf1f0f6b98e3465'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+
+  softwareName  = 'FastStone Image Viewer'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  
+
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
